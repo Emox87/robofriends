1daea9e4991eae9e1504265d67a0d567324c6bbf
Emil Tsvetanov <tsvetanovemil87@gmail.com>
2024-02-22 17:59:03 +0200

    Creating and connecting to redux store

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,36 +1,50 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
+
+import { setSearchField } from "../actions";
+
+const mapStateToProps = (state) => {
+  return {
+    searchField: state.searchField,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+  };
+};
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       robots: [],
-      searchField: "",
     };
   }
 
-  onSearchChange = (event) => {
-    this.setState({ searchField: event.target.value });
-  };
-
   render() {
-    const { searchField, robots } = this.state;
+    const { robots } = this.state;
+    const { searchField, onSearchChange } = this.props;
     const filteredRobots = robots.filter((robot) => {
       return robot.name.toLowerCase().includes(searchField.toLowerCase());
     });
 
-    return (
+    return !robots.length ? (
+      <h1>Loading</h1>
+    ) : (
       <div className="app">
-        <SearchBox searchChange={this.onSearchChange} />
+        <SearchBox searchChange={onSearchChange} />
         <CardList robots={filteredRobots} />
       </div>
     );
   }
 
   componentDidMount() {
+    // console.log(this.props.store.getState());
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
       .then((data) => this.setState({ robots: data }))
@@ -38,4 +52,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider, connect } from "react-redux";
+import { createStore } from "redux";
+import { searchRobots } from "./reducers";
+
 import "./index.css";
 import App from "./containers/App";
 
+const store = createStore(searchRobots);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    (<App />
+  </Provider>
+);
